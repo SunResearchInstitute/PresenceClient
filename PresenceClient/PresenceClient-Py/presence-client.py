1c73d00589a34cd110013d8d24c4fcb28970d2b8
LeNooby 09 <phillihp.steingraeber@gmail.com>
2023-12-15 23:08:19 +0100

    improved readability

--- PresenceClient/PresenceClient-Py/presence-client.py
+++ PresenceClient/PresenceClient-Py/presence-client.py
@@ -9,24 +9,26 @@ import os
 from pypresence import Presence
 from dotenv import load_dotenv
 
-load_dotenv() # load environment variables from .env file
+load_dotenv()
 
-switch_ip = os.getenv('IP') #gets the IP from the environment variable                          #
-client_id = os.getenv('APPLICATION_ID') #gets the client ID from the environment variable       # Made them global variables so they can be used in the Title class
-                                                                                                #
-rpc = Presence(str(client_id))                                                                  # also rpc is here to clear and close it in the title class(if that deosn't work the script will restart lol)
+switch_ip = os.getenv('IP')
+client_id = os.getenv('APPLICATION_ID') 
+
+rpc = Presence(str(client_id))
 
 TCP_PORT = 0xCAFE
 PACKETMAGIC = 0xFFAADD23
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ignore-home-screen', dest='ignore_home_screen', action='store_true', help='Don\'t display the home screen. Defaults to false if missing this flag.')
-parser.add_argument('--ignore-tinfoil', dest='ignore_tinfoil', action='store_true', help='Don\'t display the Tinfoil app. Defaults to false if missing this flag.')
-
+parser.add_argument('--ignore-home-screen', dest='ignore_home_screen', action='store_true', help='Hide the home screen. Defaults to false if missing this flag.')
+parser.add_argument('--ignore-tinfoil', dest='ignore_tinfoil', action='store_true', help='Hide the Tinfoil app. Defaults to false if missing this flag.')
 consoleargs = parser.parse_args()
 
 questOverrides = None
 switchOverrides = None
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+switch_server_address = (switch_ip, TCP_PORT)
 
 try: 
     questOverrides = json.loads(requests.get("https://raw.githubusercontent.com/Sun-Research-University/PresenceClient/master/Resource/QuestApplicationOverrides.json").text)
@@ -36,41 +38,37 @@ except:
     exit()
 
 def restart():
-    if consoleargs.ignore_tinfoil and consoleargs.ignore_home_screen:
-        os.system('python3 presence-client.py --ignore-home-screen --ignore-tinfoil')
-    if consoleargs.ignore_tinfoil:
-        os.system('python3 presence-client.py --ignore-tinfoil')
-    if consoleargs.ignore_home_screen:
-        os.system('python3 presence-client.py --ignore-home-screen')
-    os.system('python3 presence-client.py') 
+    command = 'python3 presence-client.py'
 
-#Defines a title packet
+    if consoleargs.ignore_tinfoil:
+        command += ' --ignore-tinfoil'
+    if consoleargs.ignore_home_screen:
+        command += ' --ignore-home-screen'
+
+    os.system(command)
+
 class Title:
     def __init__(self, raw_data):
-        if len(raw_data) != 628:    #checks if the data is the correct length
-            rpc.clear() #clears the RPC
-            rpc.close() #closes the RPC
-            restart() #restarts the script with the flags
+        if len(raw_data) != 628:
+            rpc.clear()
+            sock.close()
+            sock.connect(switch_server_address)
+            sock.recv(628)
+
         unpacker = struct.Struct('2L612s')
         enc_data = unpacker.unpack(raw_data)
+
         self.magic = int(enc_data[0])
-        if int(enc_data[1]) == 0:
-            self.pid = int(enc_data[1])
-            self.name = 'Home Menu'
-        if int(enc_data[1]) == 0:
-            self.pid = int(enc_data[1])
-            self.name = 'Tinfoil'
+        self.pid = int(enc_data[1])
+
+        if self.pid == 0:
+            self.name = 'Home Menu' if self.magic != PACKETMAGIC else 'Tinfoil'
         else:
-            self.pid = int(enc_data[1])
             self.name = enc_data[2].decode('utf-8', 'ignore').split('\x00')[0]
-        if int(enc_data[0]) == PACKETMAGIC:
-            if self.name in questOverrides:
-                if questOverrides[self.name]['CustomName'] != '':
-                    self.name = questOverrides[self.name]['CustomName']
-        else:
-            if self.name in switchOverrides:
-                if switchOverrides[self.name]['CustomName'] != '':
-                    self.name = switchOverrides[self.name]['CustomName']
+
+        overrides = questOverrides if self.magic == PACKETMAGIC else switchOverrides
+        if overrides is not None and self.name in overrides and overrides[self.name]['CustomName']:
+            self.name = overrides[self.name]['CustomName']
 
 def main():
     if not checkIP(switch_ip):
@@ -82,73 +80,59 @@ def main():
         rpc.clear()
     except:
         print('Unable to start RPC!')
-    switch_server_address = (switch_ip, TCP_PORT)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
         try:
             sock.connect(switch_server_address)
-            print('Successfully connected to %s' % switch_ip + ':' + str(TCP_PORT))
+            print(f'Successfully connected to {switch_ip}:{TCP_PORT}')
             break
-        except socket.error as e:
+        except socket.error:
             print(f'Error connecting to {switch_ip}:{TCP_PORT}. Retrying in 1 minute.')
-            time.sleep(60) #wait 1 minute before retrying
+            time.sleep(60)
     lastProgramName = ''
     startTimer = 0
     while True:
         data = None
         try:
             data = sock.recv(628)
-        except socket.error as e:
+        except socket.error:
             print('Could not connect to Server! Retrying...')
-            time.sleep(1)
+            time.sleep(60)
             try:
                 sock.connect(switch_server_address)
                 print('Successfully reconnected to %s' % repr(switch_server_address))
-            except socket.error as e:
+            except socket.error:
                 print(f'Error reconnecting to {repr(switch_server_address)}. Retrying...')
-                time.sleep(1)
+                time.sleep(60)
                 continue
+        def get_details(title, overrides):
+            if title.name in overrides:
+                orinfo = overrides[title.name]
+                largeimagekey = orinfo['CustomKey'] or iconFromPid(title.pid)
+                details = orinfo['CustomPrefix'] or 'Playing'
+            else:
+                largeimagekey = iconFromPid(title.pid) if int(title.pid) != PACKETMAGIC else title.name.lower().replace(' ', '')
+                details = 'Playing'
+            details += ' ' + title.name
+            return largeimagekey, details
+
         title = Title(data)
         if not hasattr(title, 'magic'): 
             restart()
             continue
-        title = Title(data)
+
         if title.magic == PACKETMAGIC:
             if lastProgramName != title.name:
                 startTimer = int(time.time())
             if consoleargs.ignore_home_screen and title.name == 'Home Menu':
                 rpc.clear()
-            if consoleargs.ignore_tinfoil and title.name == 'Tinfoil':
+            elif consoleargs.ignore_tinfoil and title.name == 'Tinfoil':
                 rpc.clear()
             else:
-                smallimagetext = ''
-                largeimagekey = ''
-                details = ''
-                largeimagetext = title.name
-                if int(title.pid) != PACKETMAGIC:
-                    smallimagetext = 'SwitchPresence-Rewritten'
-                    if title.name not in switchOverrides:
-                        largeimagekey = iconFromPid(title.pid)
-                        details = 'Playing ' + str(title.name)
-                    else:
-                        orinfo = switchOverrides[title.name]
-                        largeimagekey = orinfo['CustomKey'] or iconFromPid(title.pid)
-                        details = orinfo['CustomPrefix'] or 'Playing'
-                        details += ' ' + title.name
-                else:
-                    smallimagetext = 'QuestPresence'
-                    if title.name not in questOverrides:
-                        largeimagekey = title.name.lower().replace(' ', '')
-                        details = 'Playing ' + title.name
-                    else:
-                        orinfo = questOverrides[title.name]
-                        largeimagekey = orinfo['CustomKey'] or title.name.lower().replace(
-                            ' ', '')
-                        details = orinfo['CustomPrefix'] or 'Playing'
-                        details += ' ' + title.name
-                if not title.name:
-                    title.name = ''
-                lastProgramName = title.name
+                smallimagetext = 'SwitchPresence-Rewritten' if int(title.pid) != PACKETMAGIC else 'QuestPresence'
+                overrides = switchOverrides if int(title.pid) != PACKETMAGIC else questOverrides
+                largeimagekey, details = get_details(title, overrides)
+                largeimagetext = title.name or ''
+                lastProgramName = title.name or ''
                 rpc.update(details=details, start=startTimer, large_image=largeimagekey,
                         large_text=largeimagetext, small_text=smallimagetext)
             time.sleep(1)
